7ffd5294594990560971aced4688f3052066b3f0
KillerJV <killerhomage@gmail.com>
2020-06-05 16:46:39 -0300

    Primeiro commit

---
+++ src/main/java/com/app/ERPsis/ErPsisApplication.java
@@ -0,0 +1,13 @@
+package com.app.ERPsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErPsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErPsisApplication.class, args);
+	}
+
+}
