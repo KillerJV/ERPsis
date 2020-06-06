f7ce7b53ffe925efd82104679e87c170a9abd904
KillerJV <killerhomage@gmail.com>
2020-06-06 09:55:05 -0300

    Alteração do properties

--- src/main/java/com/app/ERPsis/model/User.java
+++ src/main/java/com/app/ERPsis/model/User.java
@@ -1,12 +1,22 @@
 package com.app.ERPsis.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Data
+@Entity
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -21,46 +31,6 @@ public class User implements Serializable {
         this.name = name;
         this.email = email;
         this.phone = phone;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
---
+++ src/main/java/com/app/ERPsis/repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package com.app.ERPsis.repositories;
+
+public class UserRepository {
+}
