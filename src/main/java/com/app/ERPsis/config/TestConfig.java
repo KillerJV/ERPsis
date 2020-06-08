59693f85ac88f723271aca14169b5872755d1172
KillerJV <killerhomage@gmail.com>
2020-06-08 16:27:52 -0300

    Implementação OrderStatus-enum

--- src/main/java/com/app/ERPsis/config/TestConfig.java
+++ src/main/java/com/app/ERPsis/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.app.ERPsis.config;
 
+import com.app.ERPsis.enums.OrderStatus;
 import com.app.ERPsis.model.Order;
 import com.app.ERPsis.model.User;
 import com.app.ERPsis.repositories.OrderRepository;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-11-09T12:30:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2018-10-09T07:55:08Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-09T10:18:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-11-09T12:30:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2018-10-09T07:55:08Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-09T10:18:22Z"), OrderStatus.SHIPPED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
