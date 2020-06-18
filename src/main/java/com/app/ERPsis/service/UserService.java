b186bd22737546e0483b5b79d99a162f69c833d9
killerJV <killerhomage@gmail.com>
2020-06-17 22:14:19 -0300

    DELETE e UPDATE User - feito

--- src/main/java/com/app/ERPsis/service/UserService.java
+++ src/main/java/com/app/ERPsis/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,4 +28,21 @@ public class UserService {
     public User insert(User obj){
         return userRepository.save(obj);
     }
+
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = userRepository.getOne(id);
+        updateData(entity, obj);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
 }
